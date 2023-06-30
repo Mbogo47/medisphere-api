1ebec0578f0707763faf05f3c53cfd486914f053
Mbogo47 <njokiivy@gmail.com>
2023-06-30 19:06:29 +0300

    changes

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors(
     {
         credentials: true,
-        origin: "http://medisphereapi.azurewebsites.net"
+        origin: "https://medisphereapi.azurewebsites.net"
     }
 ));
 
